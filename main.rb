e416900496187b63ebb4545985044cc7bcb493fb
BattyBoi69 <101750118+BattyBoi69@users.noreply.github.com>
2024-09-20 15:24:57 +0100

    SetUp: Game class structure

--- main.rb
+++ main.rb
@@ -2,7 +2,25 @@ require_relative 'lib/human_player'
 require_relative 'lib/computer_player'
 
 class Game
+  COLOURS = ["", " red  ", " blue ", "green ", " pink ", " navy ", "orange", "yellow", "white ", "black "] #index starts at 1 to make it easier
+
   def initialize
     puts "Hi"
+  end
+
+  def play
+    p COLOURS
+  end
+
+  def win?
+  end
+
+  def is_exact_match?
+  end
+
+  def is_fuzzy_match?
+  end
+
+  def print_board
   end
 end
