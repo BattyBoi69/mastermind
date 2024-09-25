fb7515049387fbee0cf1c08541a4fb33a746b97f
BattyBoi69 <101750118+BattyBoi69@users.noreply.github.com>
2024-09-25 13:58:46 +0100

    Feat: Human player choice method

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -3,5 +3,15 @@ require_relative 'player'
 class HumanPlayer < Player
   def make_guess
     super
+    guess = []
+    4.times do |i|
+      puts "Type the colour in slot #{i}:"
+      loop do
+        ith_guess = gets.chomp
+        break unless @@colours_trimmed[1..-1].none?(ith_guess)
+      end
+      guess[i-1] = @@colours_trimmed.index(ith_guess)
+    end
+    guess
   end
 end
