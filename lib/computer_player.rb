f7548fb07653d7d8fcb452bf4f0c8b104cfdbf6c
BattyBoi69 <101750118+BattyBoi69@users.noreply.github.com>
2024-09-27 08:53:31 +0100

    Feat: Computer guess using prev

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -3,16 +3,19 @@ require_relative 'player'
 class ComputerPlayer < Player
   def make_guess
     game_board = @game.board.compact
-    return [1, 1, 1, 1] if game_board.length == 0
-    return random_code(4) if game_board.length == 1
+    return random_code(4) if game_board.length <= 1
     
     persistent = persistent_colours
     num_of_persistent = persistent.length
     num_of_hits = game_board[-1][:score].delete("-").length
+    
+    num_of_psample = [num_of_persistent, num_of_hits].min
+    num_of_hsample = num_of_hits - num_of_psample
     num_of_miss = 4 - num_of_hits
 
     guess = random_code(num_of_miss)
-    num_of_hits.times{ guess << game_board[-1][:guess].sample }
+    num_of_psample.times{ guess << persistent.sample }
+    num_of_hsample.times{ guess << game_board[-1][:guess].sample }
     guess.shuffle
   end
 
@@ -31,7 +34,7 @@ class ComputerPlayer < Player
     return [] if game_board.length < 2
     recent_guess = game_board[-1][:guess]
 
-    recent_guess.select{ |ith| game_board[-2][:guess].contains(ith) }
+    recent_guess.filter{ |ith| game_board[-2][:guess].include?(ith) }
   end
    
 end
